9c46b3dd14861bef82bac155882049d399979777
Andrew Reyes <Zoey.Reyes@iam.local>
2018-09-18 15:19:04 -0500

    Started Basic Placeholder Movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
-
+    public Rigidbody2D myRigidBody;
 	// Use this for initialization
 	void Start () {
-		
+        //Debug.Log("This is Start");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKey(KeyCode.D))
+        {
+            //move character to the right
+            myRigidBody.velocity = new Vector2(3, 0);
+        }
+        //Syntax for printing to console
+        //Debug.Log("Test!");
 	}
 }
