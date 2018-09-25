d3385d85dcaa48d7e6715a2c634c61857a58e4a4
Andrew Reyes <Zoey.Reyes@iam.local>
2018-09-25 14:39:05 -0500

    Followers and Refining Movement

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour {
-    public Rigidbody2D myRigidBody;
+    [SerializeField]
+    private Rigidbody2D myRigidBody;
+    [SerializeField]
+    private float speed;
 	// Use this for initialization
 	void Start () {
         //Debug.Log("This is Start");
+        speed = 3;
 	}
 	
 	// Update is called once per frame
@@ -14,21 +18,26 @@ public class PlayerCharacter : MonoBehaviour {
         if (Input.GetKey(KeyCode.D))
         {
             //move character to the right
-            myRigidBody.velocity = new Vector2(2, 0);
+            myRigidBody.velocity = new Vector2(speed, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            myRigidBody.velocity = new Vector2(-2,0);
+            myRigidBody.velocity = new Vector2(-speed,0);
         }
         if (Input.GetKey(KeyCode.Space))
         {
-            myRigidBody.velocity = new Vector2(0,2);
+            myRigidBody.velocity = new Vector2(0,speed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            myRigidBody.velocity = new Vector2(0,-2);
+            myRigidBody.velocity = new Vector2(0,-speed);
         }
         //Syntax for printing to console
         //Debug.Log("Test!");
 	}
+        private void Move()
+        {
+            transform.Translate(1, 0, 0);
+        }
+    }
 }
