9dc94621fff55225d672a7a8d9838d51e646d575
Andrew Reyes <Zoey.Reyes@iam.local>
2018-09-25 15:05:23 -0500

    Jump Edit
    Edit jump such that it can only be used every so often as well as refine the movement and jump to work simultaneously.

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -7,6 +7,10 @@ public class PlayerCharacter : MonoBehaviour {
     private Rigidbody2D myRigidBody;
     [SerializeField]
     private float speed;
+    [SerializeField]
+    private float direction;
+    [SerializeField]
+    private float jumpCooldown;
 	// Use this for initialization
 	void Start () {
         //Debug.Log("This is Start");
@@ -17,27 +21,40 @@ public class PlayerCharacter : MonoBehaviour {
 	void Update () {
         if (Input.GetKey(KeyCode.D))
         {
+            direction = 1;
+            Move();
             //move character to the right
-            myRigidBody.velocity = new Vector2(speed, 0);
+            //myRigidBody.velocity = new Vector2(speed, 0);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            myRigidBody.velocity = new Vector2(-speed,0);
+            direction = -1;
+            Move();
         }
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space)&&jumpCooldown==0)
         {
-            myRigidBody.velocity = new Vector2(0,speed);
+            Jump();
         }
         if (Input.GetKey(KeyCode.S))
         {
             myRigidBody.velocity = new Vector2(0,-speed);
         }
+        if (jumpCooldown > 0)
+        {
+            jumpCooldown--;
+        }
         //Syntax for printing to console
         //Debug.Log("Test!");
 	}
-        private void Move()
-        {
-            transform.Translate(1, 0, 0);
-        }
+    private void Move()
+    {
+        myRigidBody.velocity = new Vector2(speed*direction, myRigidBody.velocity.y);
+        //transform.Translate(speed, 0, 0);
     }
+    private void Jump()
+    {
+        myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, speed*3);
+        jumpCooldown = 105;
+    }
+    
 }
