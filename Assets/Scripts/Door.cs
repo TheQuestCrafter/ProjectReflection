b7dda905a2c49ba452d9f7ae7aab74033d457de5
TheQuestCrafter <thequestcrafter@gmail.com>
2018-11-13 00:38:06 -0600

    Door Added

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+
+    private bool isPlayerInTrigger;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Activate") && isPlayerInTrigger)
+        {
+            Debug.Log("Player Activated Door");
+            SceneManager.LoadScene("SampleScene");
+        }
+	}
+}
